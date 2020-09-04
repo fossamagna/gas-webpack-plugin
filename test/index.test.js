d15858e80fdb8f067e89e4c97150949b1713651a
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2020-09-04 09:41:50 +0900

    feat: Add development mode

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const MemoryFS = require("memory-fs");
 const test = require('tap').test;
 const GasPlugin = require('../');
-const TerserPlugin = require('terser-webpack-plugin');
+const _ = require('lodash');
 
 const options = {
   mode: 'production',
@@ -10,21 +10,19 @@ const options = {
   entry: "./main.js",
   output: {
     path: __dirname + "/output",
-    filename: "bundle.gs",
+    filename: "bundle.js",
   },
   plugins: [
     new GasPlugin()
   ],
   optimization: {
     minimize: false,
-    minimizer: [new TerserPlugin({
-      test: /\.(j|g)s(\?.*)?$/i,
-    })],
   }
 };
 
 test('gas-plugin', function(t) {
-  const compiler = webpack(options);
+  const opts = _.cloneDeep(options);
+  const compiler = webpack(opts);
   const mfs = new MemoryFS();
   compiler.outputFileSystem = mfs;
   compiler.run(function(err, stats) {
@@ -32,7 +30,7 @@ test('gas-plugin', function(t) {
     const jsonStats = stats.toJson();
     t.ok(jsonStats.errors.length === 0);
     t.ok(jsonStats.warnings.length === 0);
-    const bundle = mfs.readFileSync(__dirname + '/output/bundle.gs', 'utf8');
+    const bundle = mfs.readFileSync(__dirname + '/output/bundle.js', 'utf8');
     const output = `/**
  * Return write arguments.
  */
@@ -48,8 +46,9 @@ function minus() {
 });
 
 test('gas-plugin prepend top-level functions when minimize is enabled', function(t) {
-  options.optimization.minimize = true;
-  const compiler = webpack(options);
+  const opts = _.cloneDeep(options);
+  opts.optimization.minimize = true;
+  const compiler = webpack(opts);
   const mfs = new MemoryFS();
   compiler.outputFileSystem = mfs;
   compiler.run(function(err, stats) {
@@ -57,8 +56,34 @@ test('gas-plugin prepend top-level functions when minimize is enabled', function
     const jsonStats = stats.toJson();
     t.ok(jsonStats.errors.length === 0);
     t.ok(jsonStats.warnings.length === 0);
-    const bundle = mfs.readFileSync(__dirname + '/output/bundle.gs', 'utf8');
+    const bundle = mfs.readFileSync(__dirname + '/output/bundle.js', 'utf8');
     const output = 'function echo(){}function plus(){}function minus(){}'
+    t.ok(bundle.toString().startsWith(output), 'plugin and expected output match');
+    t.end();
+  });
+});
+
+test('gas-plugin prepend top-level functions in development mode', function(t) {
+  const opts = _.cloneDeep(options);
+  opts.mode = 'development';
+  const compiler = webpack(opts);
+  const mfs = new MemoryFS();
+  compiler.outputFileSystem = mfs;
+  compiler.run(function(err, stats) {
+    t.error(err, 'build failed');
+    const jsonStats = stats.toJson();
+    t.ok(jsonStats.errors.length === 0);
+    t.ok(jsonStats.warnings.length === 0);
+    const bundle = mfs.readFileSync(__dirname + '/output/bundle.js', 'utf8');
+    const output = `/**
+ * Return write arguments.
+ */
+function echo() {
+}
+function plus() {
+}
+function minus() {
+}`
     t.ok(bundle.toString().startsWith(output), 'plugin and expected output match');
     t.end();
   });
