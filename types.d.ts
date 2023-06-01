00132c7808ca6e195b9050f88ed350e5937d85b9
Bruno Paré-Simard <bruno.paresimard@gmail.com>
2023-06-01 09:49:45 -0400

    Update types.d.ts
    Depending on my `tsconfig.json` configuration, I had trouble importing this plugin. This version of `types.d.ts` seems to resolve (most) of them. This seems to be a recurrent issues for `Webpack` plugins in my configuration.
    
    As references to resolve this I used:
    * https://github.com/webpack-contrib/eslint-webpack-plugin/issues/16
    * https://github.com/DefinitelyTyped/DefinitelyTyped/blob/54d540ab4deb2588c0eff39dadf370cbf0a2dee4/types/css-minimizer-webpack-plugin/index.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,21 +1,25 @@
-import type { Compiler, WebpackPluginInstance } from "webpack";
+import type { Compiler } from "webpack";
 
-interface Options {
-  /**
-   * If true then generate a top level function declaration statement with comment.
-   */
-  comment?: boolean;
-  /**
-   * Array of source file paths that to generate global assignments expression from exports.* statements.
-   */
-  autoGlobalExportsFiles?: string[];
-  /**
-   * Array of path patterns to detect functions to generate top level function definitions. accept glob pattern.
-   */
-  include?: []
-}
-
-export default class implements WebpackPluginInstance {
-  constructor(options?: Options);
+declare class GasPlugin {
+  constructor(options?: GasPlugin.Options);
   apply(compiler: Compiler): void;
 }
+
+declare namespace GasPlugin {
+  interface Options {
+    /**
+     * If true then generate a top level function declaration statement with comment.
+     */
+    comment?: boolean;
+    /**
+     * Array of source file paths that to generate global assignments expression from exports.* statements.
+     */
+    autoGlobalExportsFiles?: string[];
+    /**
+     * Array of path patterns to detect functions to generate top level function definitions. accept glob pattern.
+     */
+    include?: []
+  }
+}
+
+export = GasPlugin;
